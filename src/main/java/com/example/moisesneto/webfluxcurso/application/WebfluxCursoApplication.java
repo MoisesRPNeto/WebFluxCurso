82cdc9bbdba9e99be97f2f570501b8fc4326ec9e
MoisésRP Neto <Mmoisesneto.programador@gmail.com>
2023-09-20 17:59:51 -0300

    Iniciando estrutura de pastas

---
+++ src/main/java/com/example/moisesneto/webfluxcurso/application/WebfluxCursoApplication.java
@@ -0,0 +1,13 @@
+package com.example.moisesneto.webfluxcurso.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxCursoApplication.class, args);
+	}
+
+}
